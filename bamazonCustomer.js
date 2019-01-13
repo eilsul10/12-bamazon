51b9ece12f9652946dc92ad84ad3c2b8653924d4
Eileen S <eileen.j.sul@gmail.com>
2019-01-12 20:58:41 -0600

    final with references

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-//declare NPM packages
+//declare NPM dependencies -- referenced week 13, activity 14
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require('console.table');
@@ -22,13 +22,14 @@ connection.connect(function (err) {
     if (err) throw err;
     connection.query('SELECT * FROM products', function (err, res) {
         console.table(res);
+        // referenced week 12 - activity 10
         runSearch();
     });
 });
 
-//   create function that asks which product and how many customer wants to buy
+// create function that asks which product and how many customer wants to buy
 function runSearch() {
-
+    // referenced npm documentation
     inquirer.prompt([
         {
             name: "action",
@@ -41,23 +42,21 @@ function runSearch() {
             message: "How many would you like to purchase?",
         }
     ])
+    // referenced week 12, activity 10
         .then(function (answer) {
             var productID = answer.action;
             var purchaseQuantity = answer.quantity;
             var query = 'SELECT * FROM products WHERE ?';
             connection.query(query, { item_id: answer.action }, function (err, res) {
-
+            // if enough, fulfill the order. Update SQL database and show total cost of purchase
+            // create array where it starts at [0]; referenced mySQL documentation
                 if (purchaseQuantity <= res[0].stock_quantity) {
                     console.log("Your product is in stock! The order is being placed now.");
-                    // if enough, fulfill the order. Update SQL database and show total cost of purchase
-                    // Add a console table to improve visual output
-
                     var updateQuery = 'UPDATE products SET stock_quantity ='+ (res[0].stock_quantity-purchaseQuantity) + ' WHERE item_id = '+ (productID);
                     connection.query(updateQuery, function (err, res) {
                     });
                     var totalCost = purchaseQuantity * res[0].price;
-                    console.log("The total cost of your purchase is:" + totalCost);
-                    
+                    console.log("The total cost of your purchase is:" + totalCost); 
                 }
                 // )}
                 else {
